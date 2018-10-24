93591c7baf5b0ffd35299c7a15474a517c7ad4ff
s-ashwinkumar <s.ashwinkumar2490@gmail.com>
2018-10-24 06:50:32 +0000

    add devise generated stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :applicants, controllers: {
+      sessions: 'applicants/sessions'
+  }
+  devise_for :employers, controllers: {
+      sessions: 'employers/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
