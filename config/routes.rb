1e3d6df73b87d8d34ee34cda74b612ec7b42916f
s-ashwinkumar <s.ashwinkumar2490@gmail.com>
2018-10-24 07:18:51 +0000

    Simple index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   devise_for :employers, controllers: {
       sessions: 'employers/sessions'
   }
+  root :to => "application#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
